57de997f1affb0c7c7ac885607266079ea0f0773
Yang Guan <yangguan@google.com>
2023-01-02 05:10:36 +0000

    Print INGRESS_IP

--- scripts/gping.sh
+++ scripts/gping.sh
@@ -2,4 +2,5 @@
 
 INGRESS_IP=$(kubectl get svc -n bookstoreserver bookstoreserver -o jsonpath='{.status.loadBalancer.ingress[0].ip}')
 
+echo "INGRESS_IP=${INGRESS_IP}"
 go run cmd/bookstoreclient/main.go -server ${INGRESS_IP}:8080
