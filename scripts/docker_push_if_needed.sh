2aa6ccf57bc0553ad98a686f773f27e9adae4d0d
gaoyanwang <gaoyanwang@google.com>
2023-03-03 23:04:46 +0000

    ad RBACs to create SubjectAccessReview

--- scripts/docker_push_if_needed.sh
+++ scripts/docker_push_if_needed.sh
@@ -5,15 +5,7 @@ set -e
 project=$1
 binary=$2
 tag=$3
-
-existing_tags=$(gcloud container images list-tags --filter="tags:$tag" --format=json gcr.io/$project/$binary)
-
-if [[ "$existing_tags" == "[]" ]]; then
-  printf "build and push \"$binary\""
-
+echo "the current image tag is: $tag"
   docker build -f ./cmd/$binary/Dockerfile . -t gcr.io/$project/$binary:$tag
   docker push gcr.io/$project/$binary:$tag
-else
-  printf "tag exists\n"
-fi
 
